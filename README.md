3250ac6a1c4442efea936c295a316088bdf4adbb
andrepolischuk <andre.polischuk@gmail.com>
2015-02-19 16:26:02 +0300

    0.1.0

---
+++ README.md
@@ -0,0 +1,37 @@
+# Ies
+
+  Identify IE major version
+
+## Instalation
+
+  Browser:
+
+```html
+<script src="https://cdn.rawgit.com/andrepolischuk/ies/0.1.0/dist/ies.min.js"></script>
+```
+
+  Component(1):
+
+```sh
+$ component install andrepolischuk/ies
+```
+
+  Npm:
+
+```sh
+$ npm install ies
+```
+
+## Use
+
+  Return major version number for IE and `undefined` for other
+
+```js
+if (ies < 9) {
+  // ...
+}
+```
+
+## License
+
+  MIT
---
+++ dist/ies.js
@@ -0,0 +1,123 @@
+(function umd(require){
+  if ('object' == typeof exports) {
+    module.exports = require('1');
+  } else if ('function' == typeof define && (define.amd || define.cmd)) {
+    define(function(){ return require('1'); });
+  } else {
+    this['ies'] = require('1');
+  }
+})((function outer(modules, cache, entries){
+
+  /**
+   * Global
+   */
+
+  var global = (function(){ return this; })();
+
+  /**
+   * Require `name`.
+   *
+   * @param {String} name
+   * @param {Boolean} jumped
+   * @api public
+   */
+
+  function require(name, jumped){
+    if (cache[name]) return cache[name].exports;
+    if (modules[name]) return call(name, require);
+    throw new Error('cannot find module "' + name + '"');
+  }
+
+  /**
+   * Call module `id` and cache it.
+   *
+   * @param {Number} id
+   * @param {Function} require
+   * @return {Function}
+   * @api private
+   */
+
+  function call(id, require){
+    var m = cache[id] = { exports: {} };
+    var mod = modules[id];
+    var name = mod[2];
+    var fn = mod[0];
+
+    fn.call(m.exports, function(req){
+      var dep = modules[id][1][req];
+      return require(dep ? dep : req);
+    }, m, m.exports, outer, modules, cache, entries);
+
+    // expose as `name`.
+    if (name) cache[name] = cache[id];
+
+    return cache[id].exports;
+  }
+
+  /**
+   * Require all entries exposing them on global if needed.
+   */
+
+  for (var id in entries) {
+    if (entries[id]) {
+      global[entries[id]] = require(id);
+    } else {
+      require(id);
+    }
+  }
+
+  /**
+   * Duo flag.
+   */
+
+  require.duo = true;
+
+  /**
+   * Expose cache.
+   */
+
+  require.cache = cache;
+
+  /**
+   * Expose modules
+   */
+
+  require.modules = modules;
+
+  /**
+   * Return newest require.
+   */
+
+   return require;
+})({
+1: [function(require, module, exports) {
+
+'use strict';
+
+/**
+ * User agent
+ */
+
+var ua = navigator.userAgent;
+
+/**
+ * Export version
+ */
+
+module.exports = parse();
+
+/**
+ * Get IE major version number
+ * @return {Number}
+ * @api private
+ */
+
+function parse() {
+  var msie = /MSIE.(\d+)/i.exec(ua);
+  var rv = /rv:(\d+)/i.exec(ua);
+  var version = msie || rv || undefined;
+  return version ? +version[1] : version;
+}
+
+}, {}]}, {}, {"1":""})
+);
