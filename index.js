6111a53fb47a0ec93e4fb07d4fe6bdc3f42a3ede
andrepolischuk <andre.polischuk@gmail.com>
2015-02-19 16:21:47 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+/**
+ * User agent
+ */
+
+var ua = navigator.userAgent;
+
+/**
+ * Export version
+ */
+
+module.exports = parse();
+
+/**
+ * Get IE major version number
+ * @return {Number}
+ * @api private
+ */
+
+function parse() {
+  var msie = /MSIE.(\d+)/i.exec(ua);
+  var rv = /rv:(\d+)/i.exec(ua);
+  var version = msie || rv || undefined;
+  return version ? +version[1] : version;
+}
